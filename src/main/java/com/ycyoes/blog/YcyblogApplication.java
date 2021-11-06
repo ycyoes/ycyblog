0c139eb5a85aa94918db2fa0c8613ccf2dfb53ef
ycyoes <ycyoes@163.com>
2021-11-06 17:16:47 +0800

    update path

--- src/main/java/com/ycyoes/blog/YcyblogApplication.java
+++ src/main/java/com/ycyoes/blog/YcyblogApplication.java
@@ -1,4 +1,4 @@
-package com.ycyoes.blog.ycyblog;
+package com.ycyoes.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
