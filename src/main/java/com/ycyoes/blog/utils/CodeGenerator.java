04c6a5932dfbc2a18eb1170ceb6d45804935f72d
ycyoes <ycyoes@163.com>
2021-11-06 20:00:53 +0800

    code generator

--- src/main/java/com/ycyoes/blog/utils/CodeGenerator.java
+++ src/main/java/com/ycyoes/blog/utils/CodeGenerator.java
@@ -1,5 +1,19 @@
 package com.ycyoes.blog.utils;
 
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * @description mybatis plus code generator
  * @author ycyoes
@@ -7,4 +21,92 @@ package com.ycyoes.blog.utils;
  */
 public class CodeGenerator {
 
+    public static void main(String[] args) {
+        //代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        //全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.clearProperty("user.dir");
+        gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setAuthor("ycyoes");
+        gc.setOpen(false);
+        gc.setServiceName("%Service");
+        mpg.setGlobalConfig(gc);
+
+        //数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://localhost:3306/ycyblog?useUnicode=true&useSSL=false&characterEncoding=utf8&serverTimezone=Asia/Shanghai");
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("root");
+        mpg.setDataSource(dsc);
+
+        //包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setModuleName(null);
+        pc.setParent("com.ycyoes.blog");
+        mpg.setPackageInfo(pc);
+
+        //自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+
+            }
+        };
+
+        //如果模板引擎是freemarker
+        String templatePath = "/templates/mapper.xml.ftl";
+        //如果模板引擎是velocity
+//        String templatePath = "/templates/mapper.xml.vm";
+
+        //自定义输出配置
+        List<FileOutConfig> focList = new ArrayList<>();
+        //自定义配置会被优先输出
+        focList.add(new FileOutConfig() {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                //自定义输出文件名，如果Entity设置了前后缀，此处xml名称会跟着发生变化
+                return projectPath + "/src/main/resources/mapper"
+                        + "/" + tableInfo.getEntityName() + "Mapper"
+                        + StringPool.DOT_XML;
+            }
+        });
+
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+
+        //配置模板
+        TemplateConfig templateConfig = new TemplateConfig();
+        templateConfig.setXml(null);
+        mpg.setTemplate(templateConfig);
+
+        //策略配置
+        StrategyConfig strategyConfig = new StrategyConfig();
+        strategyConfig.setNaming(NamingStrategy.underline_to_camel);
+        strategyConfig.setEntityLombokModel(true);
+        strategyConfig.setRestControllerStyle(true);
+        strategyConfig.setInclude(scanner("表名，多个英文逗号分割").split(","));
+        strategyConfig.setControllerMappingHyphenStyle(true);
+        strategyConfig.setTablePrefix("m_");
+        mpg.setStrategy(strategyConfig);
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
+
+    //读取控制台内容
+    public static String scanner(String tip) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder help = new StringBuilder();
+        help.append("请输入" + tip + ": ");
+        System.out.println(help.toString());
+        if (scanner.hasNext()) {
+            String ipt = scanner.next();
+            if (StringUtils.isNotBlank(ipt)) {
+                return ipt;
+            }
+        }
+        throw new MybatisPlusException("请输入正确的" + tip + "! ");
+    }
 }
