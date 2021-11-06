0a4d06cc817cf9341e107c9e67d27c1203598430
ycyoes <ycyoes@163.com>
2021-11-06 17:19:36 +0800

    mybatis plus configuration

--- src/main/java/com/ycyoes/blog/config/MybatisPlusConfig.java
+++ src/main/java/com/ycyoes/blog/config/MybatisPlusConfig.java
@@ -1,4 +1,18 @@
 package com.ycyoes.blog.config;
 
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.ycyoes.blog.mapper")
 public class MybatisPlusConfig {
+    @Bean
+    public PaginationInnerInterceptor paginationInnerInterceptor() {
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+        return paginationInnerInterceptor;
+    }
 }
