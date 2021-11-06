68c791c9fc0295d38a80c0babb134c0f621dda36
ycyoes <ycyoes@163.com>
2021-11-06 16:57:58 +0800

    init

---
+++ src/main/java/com/ycyoes/blog/ycyblog/YcyblogApplication.java
@@ -0,0 +1,13 @@
+package com.ycyoes.blog.ycyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YcyblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YcyblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ycyoes/blog/ycyblog/YcyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycyoes.blog.ycyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YcyblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
