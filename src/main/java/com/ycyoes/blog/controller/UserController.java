51d6696cf571a3a5e3893ae2bba3327894e7757d
ycyoes <ycyoes@163.com>
2021-11-06 20:16:07 +0800

    get user info by id

--- src/main/java/com/ycyoes/blog/controller/UserController.java
+++ src/main/java/com/ycyoes/blog/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.ycyoes.blog.controller;
 
 
+import com.ycyoes.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -17,4 +21,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/id")
+    public Object test(@PathVariable("id") Long id) {
+        return userService.getById(id);
+    }
 }
