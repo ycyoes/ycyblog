cb6c90b536179db3155388ef443a2d8206d76a99
ycyoes <ycyoes@163.com>
2021-11-07 10:52:22 +0800

    validate login

---
+++ src/main/java/com/ycyoes/blog/common/dto/LoginDto.java
@@ -0,0 +1,14 @@
+package com.ycyoes.blog.common.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class LoginDto {
+    @NotBlank(message = "昵称不能为空")
+    private String username;
+
+    @NotBlank(message = "密码不能为空")
+    private String password;
+}
