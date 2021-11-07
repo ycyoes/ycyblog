cb2d1362051dfb67f0c1b988aceb2718665b92f6
ycyoes <ycyoes@163.com>
2021-11-07 10:51:32 +0800

    common result

--- src/main/java/com/ycyoes/blog/common/lang/Result.java
+++ src/main/java/com/ycyoes/blog/common/lang/Result.java
@@ -13,37 +13,33 @@ import lombok.Data;
 
 @Data
 public class Result {
-    private String code;
+    private int code;
     private String msg;
     private Object data;
 
     public static Result success(Object data) {
-        Result m = new Result();
-        m.setCode("0");
-        m.setData(data);
-        m.setMsg("操作成功");
-        return m;
+        return success(200, "操作成功", data);
     }
 
-    public static Result success(String msg, Object data) {
+    public static Result success(int code, String msg, Object data) {
         Result m = new Result();
-        m.setCode("0");
+        m.setCode(code);
         m.setData(data);
         m.setMsg(msg);
         return m;
     }
 
     public static Result fail(String msg) {
-        Result m = new Result();
-        m.setCode("-1");
-        m.setData(null);
-        m.setMsg(msg);
-        return m;
+        return fail(400, msg, null);
     }
 
     public static Result fail(String msg, Object data) {
+        return fail(400, msg, data);
+    }
+
+    public static Result fail(int code, String msg, Object data) {
         Result m = new Result();
-        m.setCode("-1");
+        m.setCode(code);
         m.setData(data);
         m.setMsg(msg);
         return m;
