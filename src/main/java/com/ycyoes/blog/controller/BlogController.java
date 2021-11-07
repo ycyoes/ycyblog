990cbb63e98504d39989b6a97ff02830c35228e1
ycyoes <ycyoes@163.com>
2021-11-07 11:40:25 +0800

    controller of blog

--- src/main/java/com/ycyoes/blog/controller/BlogController.java
+++ src/main/java/com/ycyoes/blog/controller/BlogController.java
@@ -1,9 +1,21 @@
 package com.ycyoes.blog.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.lang.Assert;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.ycyoes.blog.common.lang.Result;
+import com.ycyoes.blog.entity.Blog;
+import com.ycyoes.blog.service.BlogService;
+import com.ycyoes.blog.utils.ShiroUtil;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -16,5 +28,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/blog")
 public class BlogController {
+
+    @Autowired
+    BlogService blogService;
+
+    @GetMapping("/blogs")
+    public Result list(@RequestParam(defaultValue = "1") Integer currentPage) {
+        Page page = new Page(currentPage, 5);
+        IPage pageData = blogService.page(page, new QueryWrapper<Blog>().orderByDesc("create_date"));
+        return Result.success(pageData);
+    }
+
+    @GetMapping("/{id}")
+    public Result detail(@PathVariable(name = "id") Long id) {
+        Blog blog = blogService.getById(id);
+        Assert.notNull(blog, "该博客已被删除");
+        return Result.success(blog);
+    }
+
+    @RequiresAuthentication
+    @PostMapping("/edit")
+    public Result edit(@Validated @RequestBody Blog blog) {
+        Blog temp = null;
+        if (blog.getId() != null) {
+            temp = blogService.getById(blog.getId());
+            //只能编辑自己的文章
+            System.out.println(ShiroUtil.getProfile().getId());
+            Assert.isTrue(temp.getUserId().longValue() == ShiroUtil.getProfile().getId().longValue(), "没有权限编辑");
+        } else {
+            temp = new Blog();
+            temp.setUserId(ShiroUtil.getProfile().getId());
+            temp.setCreateUser("system");
+            temp.setCrateDate(LocalDateTime.now());
+            temp.setStatus(0);
+        }
+
+        BeanUtil.copyProperties(blog, temp, "id", "userId", "createDate", "status", "createUser");
+        blogService.saveOrUpdate(temp);
+        return  Result.success(null);
+    }
 
 }
